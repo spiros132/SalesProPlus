7c28c00820f52834cd86326c96b0c44dd5f624cd
Elias Nilsson <elinil0221@gmail.com>
2024-09-24 18:30:16 +0200

    fix

--- backend/api.py
+++ backend/api.py
@@ -1,41 +1,59 @@
 from fastapi import FastAPI
+import uvicorn
 import sqlite3
 
-db = sqlite3.connect("test.db")
 app = FastAPI()
 
 @app.get("/search/{prompt}")
 def search_prompt(prompt: str):
 
-    with db.cursor() as cur:
-        res = cur.execute("""
-            select * from Products where description like ?
-            """, prompt)
-        
-    return res.Rows
+    db = sqlite3.connect("test.db")
+
+    cursor = db.cursor()
+    
+    cursor.execute("""
+        SELECT 
+            p.articleID 
+        FROM Products p
+        JOIN ProductsFTS s ON p.articleID = s.articleID 
+        WHERE ProductsFTS MATCH ?
+        """, (prompt,))
+    
+    result = cursor.fetchall()
+    cursor.close()
+    db.close()
+
+    return result
     
 @app.get("/product/{id}")
 def product_page(id: int):
 
-    with db.cursor() as cur:
-        res = cur.execute("""
-            select * from ProductInformation where ArticleId = ?
-            """, id)
+    db = sqlite3.connect("test.db")
+
+    cursor =  db.cursor()
+    cursor.execute("""
+        SELECT
+            p.*, 
+            pi.info_description, 
+            pi.designer, 
+            pi.info, 
+            pi.material, 
+            pi.safety, 
+            pi.manuals, 
+            pd.dimensions, 
+            pd.packaging
+        FROM Products p
+        LEFT JOIN ProductInformation pi ON p.articleID = pi.articleID
+        LEFT JOIN ProductDimensions pd ON p.articleID = pd.articleID
+        WHERE p.articleID = ?
+        """, (id,))
+    
+    result = cursor.fetchone()
+    cursor.close()
+    db.close()
         
-    return res.Rows
+    return result 
 
 
-
-
-
- 
-
-
-cursor = db.cursor()
-
-""" with open('insert.sql', 'r') as sql_file:
-    sql_script = sql_file.read()
-
- cursor.executescript(sql_script) """
-
-db.close()
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- backend/tables.sql
+++ backend/tables.sql
@@ -7,7 +7,7 @@ CREATE TABLE Products (
 
 CREATE TABLE ProductInformation (
     articleID INT PRIMARY KEY,
-    description TEXT,
+    info_description TEXT,
     designer TEXT,
     info TEXT,
     material TEXT,
@@ -23,3 +23,32 @@ CREATE TABLE ProductDimensions (
     FOREIGN KEY (articleID) REFERENCES Products(articleID)
 );
 
+CREATE VIRTUAL TABLE ProductsFTS USING fts5 (
+    articleID,
+    name,
+    description
+);
+
+CREATE TRIGGER after_products_insert_update
+AFTER INSERT ON Products
+BEGIN
+    INSERT INTO ProductsFTS(articleID, name, description) 
+    VALUES (new.articleID, new.name, new.description);
+END;
+
+CREATE TRIGGER after_products_update
+AFTER UPDATE ON Products
+BEGIN
+    UPDATE ProductsFTS
+    SET 
+        name = new.name, 
+        description = new.description
+    WHERE articleID = new.articleID;
+END;
+
+CREATE TRIGGER after_products_delete
+AFTER DELETE ON Products
+BEGIN
+    DELETE FROM ProductsFTS 
+    WHERE articleID = old.articleID;
+END;
