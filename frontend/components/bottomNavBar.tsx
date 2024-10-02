809f6a136d6136bbde8b9e9848f1ac9c4e464b26
sanderc1 <sanderc@student.chalmers.se>
2024-10-02 02:52:30 +0200

    transition time

--- frontend/components/bottomNavBar.tsx
+++ frontend/components/bottomNavBar.tsx
@@ -26,7 +26,7 @@ function BottomNavBar() {
         <nav className="fixed flex bottom-0 left-0 right-0 bg-white text-black, h-[12vh] border-t-3 border-navBorderGray">
             <div className="flex flex-1 h-full items-center justify-center">
                 <Link className={`h-[10vh] w-[10vh] rounded-[35%] flex flex-col items-center justify-center text-black hover:bg-activeNavGray
-                    transition duration-600 ease-in-out ${active === 'Browse' ? 'bg-activeNavGray' : 'bg-white'} p-5`}  href="/" onClick={() => setActive("Browse")}>
+                    transition duration-300 ease-in-out ${active === 'Browse' ? 'bg-activeNavGray' : 'bg-white'} p-5`}  href="/" onClick={() => setActive("Browse")}>
                     <div className="w-full h-full"><SearchIcon></SearchIcon></div>
                     <p className="text-md pb-1 font-semibold text-iconGray">Browse</p>
                 </Link>                    
@@ -34,14 +34,14 @@ function BottomNavBar() {
             </div>
             <div className="flex flex-1 h-full items-center justify-center">
                 <Link className={`h-[10vh] w-[10vh] rounded-[30%] flex flex-col items-center justify-center text-black hover:bg-activeNavGray
-                    transition duration-600 ease-in-out ${active === 'Chat' ? 'bg-activeNavGray' : 'bg-white'} p-5`} href="/" onClick={() => setActive("Chat")}>
+                    transition duration-300 ease-in-out ${active === 'Chat' ? 'bg-activeNavGray' : 'bg-white'} p-5`} href="/" onClick={() => setActive("Chat")}>
                     <div className="w-full h-full"><ChatIcon></ChatIcon></div>
                     <p className="text-md pb-1 font-semibold text-iconGray">Chat</p>
                 </Link>  
             </div>
             <div className="flex flex-1 h-full items-center justify-center">
                 <Link className={`h-[10vh] w-[10vh] aspect-[1/1] rounded-[30%] flex flex-col items-center justify-center text-black hover:bg-activeNavGray
-                    transition duration-600 ease-in-out ${active === 'Settings' ? 'bg-activeNavGray' : 'bg-white'} p-5`} href="/" onClick={() => setActive("Settings")}>
+                    transition duration-300 ease-in-out ${active === 'Settings' ? 'bg-activeNavGray' : 'bg-white'} p-5`} href="/" onClick={() => setActive("Settings")}>
                     <div className="w-full h-full"><SettingsIcon></SettingsIcon></div>
                     <p className="text-md pb-1 font-semibold text-iconGray">Settings</p>
                 </Link>
