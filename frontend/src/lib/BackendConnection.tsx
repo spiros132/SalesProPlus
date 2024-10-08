e0a194441035c1b403634c2ffcc7b9ea7ad8ca40
Spyridon Siarapis <spiros.siarapis@gmail.com>
2024-10-08 17:35:00 +0200

    Fixed the api so that the cors policy is fine, and added basic functions for connecting to the backend

--- frontend/src/lib/BackendConnection.tsx
+++ frontend/src/lib/BackendConnection.tsx
@@ -1,16 +1,20 @@
 'use client'
 
-const backendURL = "http://localhost";
-const port = ":8000";
+const backendURL = "http://localhost:8000";
 
-export async function CheckBackend(): Promise<string> {
-    const data: Response = await fetch(backendURL + port,
-        {
-            mode: 'same-origin'
-        }
-    );
+export async function CheckBackend(): Promise<boolean> {
+    const data: Response = await fetch(backendURL);
     
     // Check so that the request works
-    return data.text();
-    
+    if(data.status == 200) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export async function GetProduct(productID: number) {
+    const data: Response = await fetch(backendURL + `/product/${productID}`);
+
+    return data.json();
 }
